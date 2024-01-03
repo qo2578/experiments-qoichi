8ff90441c917932475124155d290d5a88b50d6d1
aiperi7825 <132426791+iskenderovaAiperi@users.noreply.github.com>
2024-01-03 15:54:20 +0600

    added

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -12,7 +12,6 @@ import slide6 from "../../images/slide6.png";
 import slide7 from "../../images/slide7.png";
 import slide8 from "../../images/slide8.png";
 
-
 const Projects = ({ darkMode }) => {
   const settings = {
     draggable: true,
@@ -20,10 +19,9 @@ const Projects = ({ darkMode }) => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,  
-    autoplaySpeed: 2000, 
-    arrows: false, 
-  
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
   };
 
   return (
@@ -54,37 +52,98 @@ const Projects = ({ darkMode }) => {
           <div className="carousel-slide">
             <img src={slide6} alt="Описание слайда 6" />
           </div>
-
         </Slider>
       </div>
       <div>
-  <div className="box-project">
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-  <a href="https://stroy-mu.vercel.app/" target="_blank" rel="noopener noreferrer" className='link-project'>Stroyka Store
-    </a>
-    <a href="https://dananz-gilt.vercel.app/" target="_blank" rel="noopener noreferrer" className='link-project'>Dananz
-    </a>
-    <a href="https://u-source.vercel.app/" target="_blank" rel="noopener noreferrer" className='link-project'>U Source
-    </a>
-    <a href="" target="_blank" rel="noopener noreferrer" className='link-project'>Tomorrow Techs
-    </a>
-    <a href="https://macaronshop.ru/" target="_blank" rel="noopener noreferrer" className='link-project'>Macaron Shop
-    </a>
-    <a href="https://bronirui-online.ru/shakhmatka" target="_blank" rel="noopener noreferrer" className='link-project'>Бронируй онлайн
-    </a>
-    <a href="" target="_blank" rel="noopener noreferrer" className='link-project'>Tandyr Et
-    </a>
-    <a href="https://starwars-eight.vercel.app/" target="_blank" rel="noopener noreferrer" className='link-project'>StarWars
-    </a>
-    <a href="" target="_blank" rel="noopener noreferrer" className='link-project'>SMPL-3D-OBJ
-    </a>
-  </div>
+        <div className="box-project">
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <a
+            href="https://stroy-mu.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link-project"
+          >
+            <span>Online store</span>Stroyka Store
+          </a>
+          <a
+            href="https://dananz-gilt.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link-project"
+          >
+            <span>Landing page</span>Dananz
+          </a>
+          <a
+            href="https://u-source.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link-project"
+          >
+            <span>Portfolio</span>U Source
+          </a>
+          <a
+            href="https://u-app-orcin.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link-project"
+          >
+            <span>Portfolio</span>U Source 2.0
+          </a>
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link-project"
+          >
+            <span>Portfolio</span>Tomorrow Techs
+          </a>
+          <a
+            href="https://macaronshop.ru/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link-project"
+          >
+            <span>Store</span>Macaron Shop
+          </a>
+          <a
+            href="https://bronirui-online.ru/shakhmatka"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link-project"
+          >
+            <span>Booking Platform</span>Бронируй онлайн
+          </a>
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link-project"
+          >
+            <span>Online menu</span>Tandyr Et
+          </a>
+          <a
+            href="https://starwars-eight.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link-project"
+          >
+            <span>HTTP</span>StarWars
+          </a>
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link-project"
+          >
+            <span>Three.js model</span>SMPL-3D-OBJ
+          </a>
+        </div>
       </div>
     </div>
   );
